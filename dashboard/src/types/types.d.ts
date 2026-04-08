a048c1c8760d26c36c38a87ca06b728c58372dec
Sofía Verdugo <sverdugoam@gmail.com>
2026-04-07 19:54:50 -0700

    fix rating property in Product interface

--- dashboard/src/types/types.d.ts
+++ dashboard/src/types/types.d.ts
@@ -10,19 +10,6 @@ declare module "my-types" {
     description: string; 
     category: string;
     image: string; 
-    rating: Rating[] ; 
+    rating: Rating; 
   } 
 } 
-
-
-// "id": 1,
-//   "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//   "price": 109.95,
-//   "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-//   "category": "men's clothing",
-//   "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-//   "rating": {
-//     "rate": 3.9,
-//     "count": 120
-//   }
-// }
